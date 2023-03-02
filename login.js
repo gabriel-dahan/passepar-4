f79ddf54a2b52452545150f1e6b7b561fbe67e99
Adrian Utge <adrianutge@gmail.com>
2023-03-02 11:49:05 +0100

    new loggin sys v1

---
+++ login.js
@@ -0,0 +1,30 @@
+function submitData(objButton) {
+    url = '/api/players/register'
+    nom = document.getElementById('name1').value;
+    email = document.getElementById('email1').value;
+    msg = document.getElementById('msg1').value;
+    console.log(nom, email, msg)
+    data = nom, email, msg
+    postdata(url, data)
+}
+
+
+
+
+
+function postdata(url, obj) {
+    fetch(url + obj, {
+            method: "POST",
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+
+            },
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
