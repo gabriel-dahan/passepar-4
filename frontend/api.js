5037527b67ba86b02ec78e4b00f9968105abd6af
thegabdoosan <gabrieldahan05@gmail.com>
2023-03-05 13:17:43 +0100

    Updated GET/POST/PUT/DELETE requests.

--- frontend/api.js
+++ frontend/api.js
@@ -2,75 +2,71 @@ const HOST = '127.0.0.1';
 const PORT = 5000
 const API_BASE = `http://${HOST}:${PORT}/api`;
 
-const __format_data = (payload) => {
+const __formatData = (payload) => {
     let formData = new FormData();
     for (const [key, value] of Object.entries(payload))
         formData.append(key, value);
     return formData
 }
 
+const __get = (endpoint) => {
+    return fetch(endpoint, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+}
+
+const __post = (endpoint, payload = null, method = 'POST') => {
+    return fetch(endpoint, {
+        method: method,
+        body: payload ? __formatData(payload) : payload
+    });
+}
+
+const __put = (endpoint, payload = null) => __post(endpoint, payload, 'PUT');
+
+const __delete = (endpoint, payload = null) => __post(endpoint, payload, 'DELETE');
+
 class Players {
     constructor() {
         this.endpoint = API_BASE + '/player';
     }
 
-
-    register(name_, email, password) {
-        let payload = {
+    async register(name_, email, password) {
+        return __post(`${this.endpoint}/register`, {
             name: name_,
             email: email,
             password: password
-        };
-        
-        return fetch(`${this.endpoint}/register`, {
-            method: "POST",
-            body: __format_data(payload)
-        }).then((res) => res.json());
+        }).then(res => res.json());
     }
 
-    login(email, password) {
-        let payload = {
+    async login(email, password) {
+        return __post(`${this.endpoint}/login`, {
             email: email,
             password: password
-        };
-        
-        return fetch(`${this.endpoint}/login`, {
-            method: "POST",
-            body: __format_data(payload)
-        }).then((res) => res.json());
+        }).then(res => res.json());
     }
 
-    search(name_ = '', email = '', limit = 10) {
+    async search(name_ = '', email = '', limit = 10) {
         let params = `?name=${name_}&email=${email}&limit=${limit}`;
-        return fetch(`${this.endpoint}/search${params}`, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then((res) => res.json());
+        return __get(`${this.endpoint}/search${params}`)
+            .then(res => res.json());
     }
 
-    get(id) {
-        return fetch(`${this.endpoint}/${id}`, {
-            method: "GET",
-            headers: { 
-                'Content-Type': 'application/json' 
-            },
-        }).then((res) => res.json());
+    async get(id) {
+        return __get(`${this.endpoint}/${id}`)
+            .then(res => res.json());
     }
 
-    update(id, name_ = null, email = null, password = null, score = null) {
-        let payload = { 
+    async update(id, name_ = null, email = null, password = null, score = null) {
+        return __put(`${this.endpoint}/${id}/update`, { 
             name: name_,
             email: email,
             password: password,
             score: score
-        };
-
-        return fetch(`${this.endpoint}/${id}/update`, {
-            method: "PUT",
-            body: __format_data(payload)
-        }).then((res) => res.json());
+        }).then(res => res.json());
     }
 
 }
@@ -81,58 +77,37 @@ class Games {
         this.endpoint = API_BASE + '/game';
     }
 
-    new() {
-        return fetch(`${this.endpoint}/new`, {
-            method: "POST"
-        }).then((res) => res.json());
+    async new() {
+        return __post(`${this.endpoint}/new`)
+            .then(res => res.json());
     }
 
-    list(onlyIds = false) {
+    async list(onlyIds = false) {
         onlyIds = 'True' ? onlyIds : 'False';
-        return fetch(`${this.endpoint}/list?onlyids=${onlyIds}`, {
-            method: "GET",
-            headers: { 
-                'Content-Type': 'application/json' 
-            },
-        }).then((res) => res.json());
+        return __get(`${this.endpoint}/list?onlyids=${onlyIds}`)
+            .then(res => res.json());
     }
 
-    get(gameKey) {
-        return fetch(`${this.endpoint}/${gameKey}`, {
-            method: "GET",
-            headers: { 
-                'Content-Type': 'application/json' 
-            },
-        }).then((res) => res.json());
+    async get(gameKey) {
+        return __get(`${this.endpoint}/${gameKey}`)
+            .then(res => res.json());
     }
 
-    addplayer(gameKey, playerId) {
-        let payload = { 
+    async addplayer(gameKey, playerId) {
+        return __post(`${this.endpoint}/${gameKey}/addplayer`, { 
             player_id: playerId
-        };
-
-        return fetch(`${this.endpoint}/${gameKey}/addplayer`, {
-            method: "POST",
-            body: __format_data(payload)
-        }).then((res) => res.json());
+        }).then(res => res.json());
     }
 
-    play(gameKey, column) {
-        let payload = { 
+    async play(gameKey, column) {
+        return __put(`${this.endpoint}/${gameKey}/play`, { 
             column: column
-        };
-
-        return fetch(`${this.endpoint}/${gameKey}/play`, {
-            method: "PUT",
-            body: __format_data(payload)
-        }).then((res) => res.json());
+        }).then(res => res.json());
     }
 
-    delete(gameKey) {
-        return fetch(`${this.endpoint}/${gameKey}/delete`, {
-            method: "DELETE",
-            body: __format_data(payload)
-        }).then((res) => res.json());
+    async delete(gameKey) {
+        return __delete(`${this.endpoint}/${gameKey}/delete`)
+            .then(res => res.json());
     }
 }
 
@@ -141,5 +116,6 @@ const Connect4API = {
     'games': new Games()
 }
 
-// Connect4API.players.register(name_ = 'TestUser', email = 'test@user.com', password = '1234')
+// -- Example --
+// Connect4API.games.delete(gameKey = "cw8g2it")
 //     .then((data) => console.log(data));
