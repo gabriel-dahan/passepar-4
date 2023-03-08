f0381d30a0a7de29617f74c640ba439743fce3f9
thegabdoosan <gabrieldahan05@gmail.com>
2023-03-08 13:40:49 +0100

    Folders organization

--- src/Main.vue
+++ src/Main.vue
@@ -1,8 +1,6 @@
 
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import Login from './components/login.vue'
+import Login from './components/forms/Login.vue'
 </script>
 
 <!-- <template>
